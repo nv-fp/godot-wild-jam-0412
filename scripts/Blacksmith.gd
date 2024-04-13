f36957e836db6a0f19203c22b96288f3695deaeb
UnseenFaith <1832323+UnseenFaith@users.noreply.github.com>
2024-04-13 14:26:59 -0400

    fix default position

--- scripts/Blacksmith.gd
+++ scripts/Blacksmith.gd
@@ -1,11 +1,11 @@
 extends CharacterBody2D
 
-@export var speed: int = 100
+@export var speed: int = 300
 
 var directions: PackedStringArray = PackedStringArray(["east", "south_east", "south", "south_west", "west", "north_west", "north", "north_east"])
 var last_direction: String = "south"
 
-func _physics_process(_delta) -> void:
+func _process(_delta) -> void:
 	var input_direction: Vector2 = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	var angle: float = input_direction.angle()
 	var step: int = snapped(angle, PI/4) / (PI/4)
@@ -18,6 +18,7 @@ func _physics_process(_delta) -> void:
 		$AnimatedSprite2D.animation = "idle_" + last_direction
 	
 	$AnimatedSprite2D.play()
+	
 	velocity = input_direction * speed
 	
 	move_and_slide()
