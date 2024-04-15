c922e47a505ae60243e3e6ac080bf8064bdc3589
UnseenFaith <1832323+UnseenFaith@users.noreply.github.com>
2024-04-15 02:49:24 -0400

    more tilemap work, and start of anvil smithing

--- scripts/Blacksmith.gd
+++ scripts/Blacksmith.gd
@@ -8,8 +8,36 @@ extends CharacterBody2D
 var scenes = {
 	"Bronze": preload("res://nodes/items/Bronze.tscn"),
 	"Gold": preload("res://nodes/items/Gold.tscn"),
-	"Diamond": preload("res://nodes/items/Diamond.tscn")
+	"Diamond": preload("res://nodes/items/Diamond.tscn"),
+	"Unfinished_Bronze_Sword": preload("res://nodes/items/Unfinished_Bronze_Sword.tscn")
+	#"Unfinished_Gold_Sword": preload("res://nodes/items/Unfinished_Bronze_Sword.tscn"),
+	#"Unfinished_Diamond_Sword": preload("res://nodes/items/Unfinished_Bronze_Sword.tscn")
 }
+
+var atlas_coords = {
+	"Bronze": Vector2(2, 0),
+	"Gold": Vector2(3, 0),
+	"Diamond": Vector2(4, 0),
+	"Anvil": Vector2(0, 0),
+	"Furnace": Vector2(0, 1)
+}
+
+var atlas_ids = {
+	"Bronze": 14,
+	"Gold": 14,
+	"Diamond": 14,
+	"Anvil": 19,
+	"Furnace": 18
+}
+
+var atlas_layers = {
+	"Bronze": 2,
+	"Gold": 2,
+	"Diamond": 2,
+	"Anvil": 1,
+	"Furnace": 1
+}
+
 
 var pickedUpItem = false
 var heldItem: Node2D
@@ -39,11 +67,12 @@ func basic_movement():
 	
 	
 func show_interact_button():
-	if interactable:
-		var tile = tilemap.local_to_map(interactable.position)
+	if interactable && !smithingItem:
+		var group = interactable.get_groups()[0]
+		var tile = tilemap.get_used_cells_by_id(atlas_layers.get(group), atlas_ids.get(group), atlas_coords.get(group))[0]
 		if tile:
 			$InteractPrompt.visible = true
-			$InteractPrompt.position = tilemap.map_to_local(tile)
+			$InteractPrompt.position = tilemap.map_to_local(tile) - Vector2(0, -10)
 		else:
 			$InteractPrompt.visible = false
 	else:
@@ -54,28 +83,57 @@ func handleInteractionInput() -> void:
 		var group = interactable.get_groups()[0]
 		match group:
 			"Diamond","Gold","Bronze":
-				if heldItem != null:
-					return
-				pickedUpItem = true
-				var sceneToCreate = scenes.get(group)
-				var node = sceneToCreate.instantiate()
-				heldItem = node
-				add_child(node)
-				node.scale.x = 0.75
-				node.scale.y = 0.75
-				node.y_sort_enabled = false
-				node.z_index = 2
-				node.position = $Marker2D.position
+				spawn_in_held_item(group)
 			"Anvil":
-				print("Anvil")
+				if heldItem == null:
+					return
+				smithingItem = true
+				remove_held_item()
+				print("Starting to smith Item!")
+				await get_tree().create_timer(5.0).timeout
+				print("Done smithing item!")
+				spawn_in_held_item("Unfinished_Bronze_Sword")
+				smithingItem = false
+
+func spawn_in_held_item(item):
+	if heldItem != null:
+		return
+	pickedUpItem = true
+	var sceneToCreate = scenes.get(item)
+	var node = sceneToCreate.instantiate()
+	heldItem = node
+	add_child(node)
+	node.scale.x = 0.25
+	node.scale.y = 0.25
+	node.y_sort_enabled = false
+	node.z_index = 2
+	node.position = $Marker2D.position
+
+func remove_held_item() -> Node2D:
+		var node = heldItem
+		heldItem = null
+		pickedUpItem = false
+		remove_child(node)
+		return node
 
 func _physics_process(_delta) -> void:
-	basic_movement()
+	if !smithingItem:
+		basic_movement()
 	show_interact_button()
 	
 func _process(_delta) -> void:
+	if smithingItem:
+		return
 	if Input.is_action_just_pressed("interaction"):
 		handleInteractionInput()
+	if Input.is_action_just_pressed("drop_item"):
+		var item = remove_held_item()
+		tilemap.add_child(item)
+		item.z_index = 1
+		item.y_sort_enabled = true
+		item.scale.x = .5
+		item.scale.y = .5
+		item.position = position
 	
 
 
--- scripts/TileMap.gd
+++ scripts/TileMap.gd
@@ -1,8 +1,9 @@
 extends TileMap
 
 var crateCollisionTiles: PackedVector2Array = PackedVector2Array([Vector2(31, -1), Vector2(31, -3), Vector2(31, -5)])
-var anvilCollisionTiles: PackedVector2Array = PackedVector2Array([Vector2(24, -5), Vector2(25, -5), Vector2(26, -5)])
-var groupNames = ["Bronze", "Gold", "Diamond", "Anvil", "Anvil", "Anvil"]
+var anvilCollisionTile: Vector2 = Vector2(25, -5)
+var furnaceCollisionTile: Vector2 = Vector2(28, -5)
+var groupNames = ["Bronze", "Gold", "Diamond"]
 var canInteract = false
 var interactables = []
 
@@ -14,24 +15,60 @@ func _ready():
 	var interactables = []
 	
 	interactables.append_array(crateCollisionTiles)
-	interactables.append_array(anvilCollisionTiles)
 	
-	for i in range(interactables.size()):
-		var area: Area2D = create_collision_for_tile(interactables[i])
+	for i in range(crateCollisionTiles.size()):
+		var area: Area2D = create_collision_for_single_tile(crateCollisionTiles[i])
 		area.body_entered.connect(area_entered.bind(area))
 		area.body_exited.connect(area_exited.bind(area))
 		
 		area.add_to_group(groupNames[i])
 		add_child(area)
+	
+	var anvilArea = create_collision_for_anvil(anvilCollisionTile)
+	anvilArea.add_to_group("Anvil")
+	anvilArea.body_entered.connect(area_entered.bind(anvilArea))
+	anvilArea.body_exited.connect(area_exited.bind(anvilArea))
+	add_child(anvilArea)
+	
+	var furnaceArea = create_collision_for_furnace(furnaceCollisionTile)
+	furnaceArea.add_to_group("Furnace")
+	furnaceArea.body_entered.connect(area_entered.bind(furnaceArea))
+	furnaceArea.body_exited.connect(area_exited.bind(furnaceArea))
+	add_child(furnaceArea)
 
 # Create Area2D on tile
-func create_collision_for_tile(coordinates: Vector2) -> Area2D:
+func create_collision_for_single_tile(coordinates: Vector2) -> Area2D:
 	var tile_position: Vector2 = map_to_local(coordinates)
-	var top: Vector2 = tile_position + Vector2(0, -16 / 2)
-	var left: Vector2 = tile_position + Vector2(-32 / 2 , 0)
+	var top: Vector2 = tile_position + Vector2(0, -12 / 2)
+	var left: Vector2 = tile_position + Vector2(-28 / 2 , 0)
+	var bottom: Vector2 = tile_position + Vector2(0, 12 / 2)
+	var right: Vector2 = tile_position + Vector2(28 / 2, 0)
+	var area: Area2D = Area2D.new()
+	area.z_index = 1
+	var collision_shape: CollisionPolygon2D = CollisionPolygon2D.new()
+	area.add_child(collision_shape)
+	collision_shape.set_polygon(PackedVector2Array([top, left, bottom, right]))
+	return area
+
+func create_collision_for_anvil(coordinates: Vector2) -> Area2D:
+	var tile_position: Vector2 = map_to_local(coordinates)
+	var top: Vector2 = tile_position - Vector2(16, 16 / 2) + Vector2(0, -16 / 2)
+	var left: Vector2 = tile_position - Vector2(16, 16 / 2) + Vector2(-32 / 2 , 0)
 	var bottom: Vector2 = tile_position + Vector2(0, 16 / 2)
 	var right: Vector2 = tile_position + Vector2(32 / 2, 0)
-		
+	var area: Area2D = Area2D.new()
+	area.z_index = 1
+	var collision_shape: CollisionPolygon2D = CollisionPolygon2D.new()
+	area.add_child(collision_shape)
+	collision_shape.set_polygon(PackedVector2Array([top, left, bottom, right]))
+	return area
+
+func create_collision_for_furnace(coordinates: Vector2) -> Area2D:
+	var tile_position: Vector2 = map_to_local(coordinates)
+	var top: Vector2 = tile_position - Vector2(16, 16 / 2) + Vector2(0, -16 / 2)
+	var left: Vector2 = tile_position - Vector2(16, 16 / 2) + Vector2(-32 / 2 , 0)
+	var bottom: Vector2 = tile_position + Vector2(16, 16 / 2) + Vector2(0, 16 / 2)
+	var right: Vector2 = tile_position + Vector2(16, 16 / 2) + Vector2(32 / 2, 0)
 	var area: Area2D = Area2D.new()
 	area.z_index = 1
 	var collision_shape: CollisionPolygon2D = CollisionPolygon2D.new()
@@ -41,23 +78,22 @@ func create_collision_for_tile(coordinates: Vector2) -> Area2D:
 
 # Old Triggger for entering a crates area2d
 func area_entered(node: Node2D, emitter: Area2D):
+	# Area2D colliding with something, check if our node is the blacksmith
+	# so that we don't prematurely set z_index
+	if node != $Blacksmith:
+		return
 	$Blacksmith.interactable = emitter
+	if (emitter.get_groups()[0] == "Furnace"):
+		$Blacksmith.z_index = 4
 
 # Old Trigger for exiting a crates area2d
 func area_exited(node: Node2D, emitter: Area2D):
 	$Blacksmith.interactable = null
+	if (emitter.get_groups()[0] == "Furnace"):
+		$Blacksmith.z_index = 1
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 # This should be moved to Blacksmith eventually probably
 func _process(_delta):
 	$Camera2D.position = $Blacksmith.position
-	if Input.is_action_just_pressed("drop_item") && $Blacksmith.pickedUpItem == true:
-		var node = $Blacksmith.heldItem
-		$Blacksmith.pickedUpItem = false
-		$Blacksmith.remove_child(node)
-		add_child(node)
-		node.z_index = 1
-		node.y_sort_enabled = true
-		node.scale.x = 1
-		node.scale.y = 1
-		node.position = $Blacksmith.position
+
