1b64082d96c83443e0f37cd93ca8401598fd6010
UnseenFaith <1832323+UnseenFaith@users.noreply.github.com>
2024-04-16 05:17:22 -0400

    fix merge

--- scripts/Blacksmith.gd
+++ scripts/Blacksmith.gd
@@ -6,60 +6,105 @@ extends CharacterBody2D
 @export var speed: int = 100
 
 var scenes = {
+<<<<<<< Updated upstream
 	"Bronze_Ore": preload("res://nodes/items/Bronze_Ore.tscn"),
 	"Gold_Ore": preload("res://nodes/items/Gold_Ore.tscn"),
 	"Diamond_Ore": preload("res://nodes/items/Diamond_Ore.tscn"),
-	"Unfinished_Bronze_Sword": preload("res://nodes/items/Unfinished_Bronze_Sword.tscn"),
-	#"Unfinished_Gold_Sword": preload("res://nodes/items/Unfinished_Bronze_Sword.tscn"),
-	#"Unfinished_Diamond_Sword": preload("res://nodes/items/Unfinished_Bronze_Sword.tscn"),
+	"Leather_Hide": preload("res://nodes/items/Leather.tscn"),
 	"Bronze_Shield_Chunk": preload("res://nodes/items/Bronze_Shield_Chunk.tscn"),
 	"Gold_Shield_Chunk": preload("res://nodes/items/Gold_Shield_Chunk.tscn"),
 	"Diamond_Shield_Chunk": preload("res://nodes/items/Diamond_Shield_Chunk.tscn"),
 	"Bronze_Shield": preload("res://nodes/items/Bronze_Shield.tscn"),
 	"Gold_Shield": preload("res://nodes/items/Gold_Shield.tscn"),
 	"Diamond_Shield": preload("res://nodes/items/Diamond_Shield.tscn"),
+	"Bronze_Blade_Chunk": preload("res://nodes/items/Bronze_Blade_Chunk.tscn"),
+	"Gold_Blade_Chunk": preload("res://nodes/items/Gold_Blade_Chunk.tscn"),
+	"Diamond_Blade_Chunk": preload("res://nodes/items/Diamond_Blade_Chunk.tscn"),
+	"Bronze_Blade": preload("res://nodes/items/Bronze_Blade.tscn"),
+	"Gold_Blade": preload("res://nodes/items/Gold_Blade.tscn"),
+	"Diamond_Blade": preload("res://nodes/items/Diamond_Blade.tscn"),
+	"Bronze_Sword": preload("res://nodes/items/Bronze_Sword.tscn"),
+	"Gold_Sword": preload("res://nodes/items/Gold_Sword.tscn"),
+	"Diamond_Sword": preload("res://nodes/items/Diamond_Sword.tscn")
 }
 
 var atlas_coords = {
 	"Bronze_Ore": Vector2(2, 0),
 	"Gold_Ore": Vector2(3, 0),
 	"Diamond_Ore": Vector2(4, 0),
+	"Leather_Hide": Vector2(0, 0),
 	"Anvil": Vector2(0, 0),
-	"Furnace": Vector2(0, 1)
+	"Furnace": Vector2(0, 1),
+	"Trash": Vector2(0, 0),
+	"Craft": Vector2(0, 0)
 }
 
 var atlas_ids = {
 	"Bronze_Ore": 14,
 	"Gold_Ore": 14,
 	"Diamond_Ore": 14,
+	"Leather_Hide": 14,
 	"Anvil": 19,
-	"Furnace": 18
+	"Furnace": 18,
+	"Trash": 9,
+	"Craft": 24
 }
 
 var atlas_layers = {
 	"Bronze_Ore": 2,
 	"Gold_Ore": 2,
 	"Diamond_Ore": 2,
+	"Leather_Hide": 2,
 	"Anvil": 1,
-	"Furnace": 1
+	"Furnace": 1,
+	"Trash": 1,
+	"Craft": 1
 }
 
-var recipes = {
-	"Bronze_Shield_Chunk": ["Bronze_Ore", "Bronze_Ore", "Bronze_Ore"],
-	"Gold_Shield_Chunk": ["Gold_Ore", "Gold_Ore", "Gold_Ore"],
-	"Diamond_Shield_Chunk": ["Diamond_Ore", "Diamond_Ore", "Diamond_Ore"],
-	"Bronze_Shield": ["Bronze_Shield_Chunk"],
-	"Gold_Shield": ["Gold_Shield_Chunk"],
-	"Diamond_Shield": ["Diamond_Shield_Chunk"]
+var item_scales = {
+	"Bronze_Ore": Vector2(0.6, 0.6),
+	"Gold_Ore": Vector2(0.6, 0.6),
+	"Diamond_Ore": Vector2(0.6, 0.6),
+	"Leather_Hide": Vector2(0.6, 0.6),
+	"Bronze_Shield_Chunk": Vector2(1, 1),
+	"Gold_Shield_Chunk": Vector2(1, 1),
+	"Diamond_Shield_Chunk": Vector2(1, 1),
+	"Bronze_Shield": Vector2(1,1),
+	"Gold_Shield": Vector2(1, 1),
+	"Diamond_Shield": Vector2(1, 1),
+	"Bronze_Blade_Chunk": Vector2(1, 1),
+	"Gold_Blade_Chunk": Vector2(1, 1),
+	"Diamond_Blade_Chunk": Vector2(1, 1),
+	"Bronze_Blade": Vector2(1, 1),
+	"Gold_Blade": Vector2(1, 1),
+	"Diamond_Blade": Vector2(1, 1),
+	"Bronze_Sword": Vector2(1, 1),
+	"Gold_Sword": Vector2(1, 1),
+	"Diamond_Sword": Vector2(1, 1)
 }
 
+var furnace_allowed_items = ["Bronze_Ore", "Gold_Ore", "Diamond_Ore"]
+var anvil_allowed_items = ["Bronze_Shield_Chunk", "Gold_Shield_Chunk", "Diamond_Shield_Chunk", "Bronze_Blade_Chunk", "Gold_Blade_Chunk", "Diamond_Blade_Chunk"]
+var table_allowed_items = ["Bronze_Blade", "Gold_Blade", "Diamond_Blade", "Leather_Hide"]
+
+=======
+	"Bronze": preload("res://nodes/items/Bronze.tscn"),
+	"Gold": preload("res://nodes/items/Gold.tscn"),
+	"Diamond": preload("res://nodes/items/Diamond.tscn")
+}
+
+>>>>>>> Stashed changes
 var pickedUpItem = false
 var heldItem: Node2D
 var interactable: Area2D
 
+<<<<<<< Updated upstream
 var smithingItem: bool = false
 var smithingTimerScene: PackedScene = preload("res://nodes/scenes/cowndown_timer.tscn")
 var smithingTimer: Node2D
+=======
+var smithingItem = false
+>>>>>>> Stashed changes
 
 var directions: PackedStringArray = PackedStringArray(["east", "south_east", "south", "south_west", "west", "north_west", "north", "north_east"])
 var last_direction: String = "south"
@@ -77,12 +122,17 @@ func basic_movement():
 	else:
 		$AnimatedSprite2D.animation = "idle_" + last_direction
 		
+<<<<<<< Updated upstream
+	$AnimatedSprite2D.play()
+=======
 	$AnimatedSprite2D.play()	
+>>>>>>> Stashed changes
 	velocity = input_direction * speed
 	move_and_slide()
 	
 	
 func show_interact_button():
+<<<<<<< Updated upstream
 	if interactable && !smithingItem:
 		var group = interactable.get_groups()[0]
 		var tile = tilemap.get_used_cells_by_id(atlas_layers.get(group), atlas_ids.get(group), atlas_coords.get(group))[0]
@@ -98,30 +148,50 @@ func handle_interaction_input() -> void:
 	if interactable:
 		var group = interactable.get_groups()[0]
 		match group:
-			"Diamond_Ore","Gold_Ore","Bronze_Ore":
-				spawn_in_held_item(group)
-			"Anvil":
-				
-				### Come back here and do something similar like I did with Furnace
+			"Diamond_Ore","Gold_Ore","Bronze_Ore","Leather_Hide":
 				if heldItem == null:
+					spawn_in_held_item(group)
+			"Anvil":
+				### Come back here and do something similar like I did with Furnace
+				var anvil = tilemap.anvils.filter(func (a): return a.area == interactable).front()
+
+				if  heldItem == null && anvil.inventory.size() == 1 && anvil.smithing:
+					clear_anvil(anvil)
 					return
-				smithingItem = true
-				remove_held_item()
-				smithingTimer = smithingTimerScene.instantiate()
-				smithingTimer.autostart = false
-				smithingTimer.connect("timeout", smithing_timer_timeout)
-				smithingTimer.run_time = 5
-				add_child(smithingTimer)
-				smithingTimer.start()
-				smithingTimer.position = get_location_from_group(interactable.get_groups()[0]) - Vector2(0, 30)
-				smithingTimer.z_index = 4
-				smithingTimer.top_level = true
-				smithingTimer.scale.x = 0.4
-				smithingTimer.scale.y = 0.4
+
+				if heldItem && !anvil.smithing:
+					var item = heldItem.get_groups()[0]
+					
+					if !anvil_allowed_items.has(item):
+						return
+
+					anvil.inventory.append(item)
+					remove_held_item()
+					anvil.recipe = "_".join(item.split("_").slice(0, 2))
+					
+					if anvil.recipes.has(anvil.recipe):
+						smithingItem = true
+						anvil.smithing = true
+						anvil.timer = smithingTimerScene.instantiate()
+						anvil.timer.autostart = false
+						anvil.timer.run_time = 5
+						add_child(anvil.timer)
+						anvil.timer.position = get_location_from_group(group, anvil.tile) - Vector2(0, 30)
+						anvil.timer.z_index = 4
+						anvil.timer.top_level = true
+						anvil.timer.scale.x = 0.4
+						anvil.timer.scale.y = 0.4
+						anvil.timer.start()
+						await anvil.timer.timeout
+						smithingItem = false
+						anvil.inventory = [anvil.recipe]
+						anvil.timer = null
 			"Furnace":
-				var furnace = tilemap.furnaces[0]
+				var furnace = tilemap.furnaces.filter(func (f): return f.area == interactable).front()
+				print(furnace)
+				
 				# Our item finished smelting and we need to collect it
-				if heldItem == null && furnace.inventory[0] == furnace.recipe:
+				if heldItem == null && furnace.inventory.size() == 1 && furnace.smelting:
 					clear_furnace(furnace)
 					return
 				
@@ -132,6 +202,9 @@ func handle_interaction_input() -> void:
 				if heldItem != null && !furnace.smelting:
 					var resource = heldItem.get_groups()[0]
 					
+					if !furnace_allowed_items.has(resource):
+						return
+					
 					if !furnace.inventory.all(func(r): return r == resource):
 						print("Not Everything inside was same, disgard interact action")
 						return
@@ -139,20 +212,70 @@ func handle_interaction_input() -> void:
 					
 					remove_held_item()
 					match furnace.inventory.size():
-						0:
-							furnace.recipe = resource.split(" ")[0] + "_Staff_Chunk"
 						1:
-							furnace.recipe = resource.split(" ")[0] + "_Sword_Chunk"
+							furnace.recipe = resource.split("_")[0] + "_Staff_Chunk"
 						2:
-							furnace.recipe = resource.split(" ")[0] + "_Shield_Chunk"
+							furnace.recipe = resource.split("_")[0] + "_Blade_Chunk"
+						3:
+							furnace.recipe = resource.split("_")[0] + "_Shield_Chunk"
+			"Craft":
+				var table = tilemap.tables.filter(func (t): return t.area == interactable).front()
+				if heldItem == null && table.inventory.size() == 1 && table.crafting:
+					clear_table(table)
+					return
+				
+				if table.inventory.size() > 1:
+					return
 					
+				if heldItem != null && !table.crafting:
+					var resource = heldItem.get_groups()[0]
+					
+					if !table_allowed_items.has(resource):
+						return
+					
+					if table.inventory.any(func(r): return r == resource):
+						print("You already put one of those in there!")
+						return
+					
+					remove_held_item()
+					
+					if resource != "Leather_Hide":
+						table.inventory.push_front(resource)
+						var material = resource.split("_")[0]
+						table.recipe = material + "_Sword"
+					else:
+						table.inventory.append(resource)
+						
+					
+					print(table.inventory)
+					print(table.recipes.get(table.recipe))
+					print(table.inventory == table.recipes.get(table.recipe))
+
+					if table.recipes.has(table.recipe) && table.inventory == table.recipes.get(table.recipe):
+						table.crafting = true
+						table.timer = smithingTimerScene.instantiate()
+						table.timer.autostart = false
+						table.timer.run_time = 5
+						add_child(table.timer)
+						table.timer.position = get_location_from_group(group, table.tile) - Vector2(0, 30)
+						table.timer.z_index = 4
+						table.timer.top_level = true
+						table.timer.scale = Vector2(0.4, 0.4)
+						table.timer.start()
+						await table.timer.timeout
+						table.inventory = [table.recipe]
+						table.timer = null
+			"Trash":
+				if heldItem != null:
+					remove_held_item()
+
 func handle_start_interaction_input():
 	if interactable:
 		var group = interactable.get_groups()[0]
 		match group:
 			"Furnace":
-				var furnace = tilemap.furnaces[0]
-				if recipes.has(furnace.recipe) && !furnace.smelting:
+				var furnace = tilemap.furnaces.filter(func (f): return f.area == interactable).front()
+				if furnace.recipes.has(furnace.recipe) && !furnace.smelting:
 					print("Starting smelting for " + furnace.recipe)
 					furnace.smelting = true
 					furnace.timer = smithingTimerScene.instantiate()
@@ -161,15 +284,16 @@ func handle_start_interaction_input():
 					furnace.timer.run_time = 5
 					add_child(furnace.timer)
 					furnace.timer.start()
-					furnace.timer.position = get_location_from_group(group) - Vector2(0, 30)
+					furnace.timer.position = get_location_from_group(group, furnace.tile) - Vector2(-10, 40)
 					furnace.timer.z_index = 4
 					furnace.timer.top_level = true
 					furnace.timer.scale.x = 0.4
 					furnace.timer.scale.y = 0.4
-					
 
-func get_location_from_group(group: String) -> Vector2:
-	var tile = tilemap.get_used_cells_by_id(atlas_layers.get(group), atlas_ids.get(group), atlas_coords.get(group))[0]
+
+func get_location_from_group(group: String, tile: Vector2) -> Vector2:
+	if !tile:
+		tile = tilemap.get_used_cells_by_id(atlas_layers.get(group), atlas_ids.get(group), atlas_coords.get(group))[0]
 	if tile:
 		if group == "Anvil":
 			return tilemap.map_to_local(tile)
@@ -178,23 +302,34 @@ func get_location_from_group(group: String) -> Vector2:
 	else:
 		return Vector2(0, 0)
 
-func smithing_timer_timeout(_id: String):
-	smithingItem = false
-	remove_child(smithingTimer)
-	smithingTimer = null
-	spawn_in_held_item("Unfinished_Bronze_Sword")
-
 func furnace_timer_timeout(_id: String, furnace):
-	furnace.smelting = false
 	var itemToSpawn = furnace.recipe
 	furnace.inventory = [furnace.recipe]
+	remove_child(furnace.timer)
 	furnace.timer = null
 
 func clear_furnace(furnace):
+	furnace.smelting = false
 	print("Picking Up Item From Furnace")
 	var item = furnace.inventory[0]
 	furnace.recipe = null
 	furnace.inventory = []
+	spawn_in_held_item(item)
+
+func clear_anvil(anvil):
+	anvil.smithing = false
+	print("Picking up Item from Anvil")
+	var item = anvil.inventory[0]
+	anvil.recipe = null
+	anvil.inventory = []
+	spawn_in_held_item(item)
+
+func clear_table(table):
+	table.crafting = false
+	print("Picking up Item from Table")
+	var item = table.inventory[0]
+	table.recipe = null
+	table.inventory = []
 	spawn_in_held_item(item)
 
 func spawn_in_held_item(item: String):
@@ -207,8 +342,7 @@ func spawn_in_held_item(item: String):
 	heldItem = node
 	node.add_to_group(item)
 	add_child(node)
-	node.scale.x = 0.25
-	node.scale.y = 0.25
+	node.scale = item_scales.get(item)
 	node.y_sort_enabled = false
 	node.z_index = 2
 	node.position = $Marker2D.position
@@ -232,15 +366,50 @@ func _process(_delta) -> void:
 		handle_interaction_input()
 	if Input.is_action_just_pressed("start_block"):
 		handle_start_interaction_input()
-	if Input.is_action_just_pressed("drop_item"):
-		var item = remove_held_item()
-		tilemap.add_child(item)
-		item.z_index = 1
-		item.y_sort_enabled = true
-		item.scale.x = .5
-		item.scale.y = .5
-		item.position = position
 	
 
 
 
+=======
+	pass
+	#if raycastResult.has('position'):
+		#var tile = tilemap.local_to_map(raycastResult.get('position'))
+		#if tile:
+			#$InteractPrompt.visible = true
+			#$InteractPrompt.position = tilemap.map_to_local(tile)
+		#else:
+			#$InteractPrompt.visible = false
+	#else:
+		#$InteractPrompt.visible = false
+
+func handleInteractionInput() -> void:
+	if interactable:
+		var group = interactable.get_groups()[0]
+		match group:
+			"Diamond","Gold","Bronze":
+				if heldItem != null:
+					return
+				pickedUpItem = true
+				var sceneToCreate = scenes.get(group)
+				var node = sceneToCreate.instantiate()
+				heldItem = node
+				add_child(node)
+				node.scale.x = 0.75
+				node.scale.y = 0.75
+				node.y_sort_enabled = false
+				node.z_index = 2
+				node.position = $Marker2D.position
+			"Anvil":
+				print("Anvil")
+
+func _physics_process(_delta) -> void:
+	basic_movement()
+	show_interact_button()
+	
+func _process(_delta) -> void:
+	if Input.is_action_just_pressed("interaction"):
+		handleInteractionInput()
+	
+
+
+>>>>>>> Stashed changes
--- scripts/Level2.gd
+++ scripts/Level2.gd
@@ -1,46 +1,27 @@
 extends TileMap
 
-<<<<<<< Updated upstream
-var crate_collision_tiles: PackedVector2Array = PackedVector2Array([Vector2(31, -1), Vector2(31, -3), Vector2(31, -5)])
-var anvil_collision_tile: Vector2 = Vector2(25, -5)
-var furnace_collision_tile: Vector2 = Vector2(28, -5)
-var trash_collision_tile: Vector2 = Vector2(25, -3)
-var groupNames = ["Bronze_Ore", "Gold_Ore", "Diamond_Ore"]
+var crate_collision_tiles: PackedVector2Array = PackedVector2Array([Vector2(35, 0), Vector2(35, -3), Vector2(29, 3), Vector2(27, 3)])
+var anvil_collision_tile: Vector2 = Vector2(25, -3)
+var furnace_collision_tiles: PackedVector2Array = PackedVector2Array([Vector2(30, -5), Vector2(26, -5)])
+var trash_collision_tile: Vector2 = Vector2(25, 1)
+var table_collision_tile: Vector2 = Vector2(35, 2)
+var groupNames = ["Bronze_Ore", "Gold_Ore", "Diamond_Ore", "Leather_Hide"]
 
 var furnaces = []
 var anvils = []
-=======
-var crateCollisionTiles: PackedVector2Array = PackedVector2Array([Vector2(31, -1), Vector2(31, -3), Vector2(31, -5)])
-var anvilCollisionTiles: PackedVector2Array = PackedVector2Array([Vector2(24, -5), Vector2(25, -5), Vector2(26, -5)])
-var groupNames = ["Bronze", "Gold", "Diamond", "Anvil", "Anvil", "Anvil"]
-var canInteract = false
-var interactables = []
-
-var tileToArea = {}
->>>>>>> Stashed changes
+var tables = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$Camera2D.position = $Blacksmith.position
-<<<<<<< Updated upstream
 
 	for i in range(crate_collision_tiles.size()):
 		var area: Area2D = create_collision_for_single_tile(crate_collision_tiles[i])
-=======
-	var interactables = []
-	
-	interactables.append_array(crateCollisionTiles)
-	interactables.append_array(anvilCollisionTiles)
-	
-	for i in range(interactables.size()):
-		var area: Area2D = create_collision_for_tile(interactables[i])
->>>>>>> Stashed changes
 		area.body_entered.connect(area_entered.bind(area))
 		area.body_exited.connect(area_exited.bind(area))
 		
 		area.add_to_group(groupNames[i])
 		add_child(area)
-<<<<<<< Updated upstream
 	
 	var anvil_area = create_collision_for_anvil(anvil_collision_tile)
 	anvil_area.add_to_group("Anvil")
@@ -49,6 +30,7 @@ func _ready():
 	add_child(anvil_area)
 	
 	anvils.append({
+		"tile": anvil_collision_tile,
 		"recipe": null,
 		"smithing": false,
 		"timer": null,
@@ -58,27 +40,56 @@ func _ready():
 		"recipes": {
 			"Bronze_Shield": ["Bronze_Shield_Chunk"],
 			"Gold_Shield": ["Gold_Shield_Chunk"],
-			"Diamond_Shield": ["Diamond_Shield_Chunk"]
+			"Diamond_Shield": ["Diamond_Shield_Chunk"],
+			"Bronze_Blade": ["Bronze_Blade_Chunk"],
+			"Gold_Blade": ["Gold_Blade_Chunk"],
+			"Diamond_Blade": ["Diamond_Blade_Chunk"]
 		}
 	})
 	
-	var furnace_area = create_collision_for_furnace(furnace_collision_tile)
-	furnace_area.add_to_group("Furnace")
-	furnace_area.body_entered.connect(area_entered.bind(furnace_area))
-	furnace_area.body_exited.connect(area_exited.bind(furnace_area))
-	add_child(furnace_area)
+	for i in range(furnace_collision_tiles.size()):
+		var furnace_area = create_collision_for_furnace(furnace_collision_tiles[i])
+		furnace_area.add_to_group("Furnace")
+		furnace_area.body_entered.connect(area_entered.bind(furnace_area))
+		furnace_area.body_exited.connect(area_exited.bind(furnace_area))
+		add_child(furnace_area)
 	
-	furnaces.append({
+		furnaces.append({
+			"tile": furnace_collision_tiles[i],
+			"recipe": null,
+			"smelting": false,
+			"timer": null,
+			"area": furnace_area,
+			"inventory": [],
+			"id": "Furnace",
+			"recipes": {
+				"Bronze_Shield_Chunk": ["Bronze_Ore", "Bronze_Ore", "Bronze_Ore"],
+				"Gold_Shield_Chunk": ["Gold_Ore", "Gold_Ore", "Gold_Ore"],
+				"Diamond_Shield_Chunk": ["Diamond_Ore", "Diamond_Ore", "Diamond_Ore"],
+				"Bronze_Blade_Chunk": ["Bronze_Ore", "Bronze_Ore"],
+				"Gold_Blade_Chunk": ["Gold_Ore", "Gold_Ore"],
+				"Diamond_Blade_Chunk": ["Diamond_Ore", "Diamond_Ore"]
+			}
+		})
+	
+	var table_area = create_collision_for_table(table_collision_tile)
+	table_area.add_to_group("Craft")
+	table_area.body_entered.connect(area_entered.bind(table_area))
+	table_area.body_exited.connect(area_exited.bind(table_area))
+	add_child(table_area)
+	
+	tables.append({
+		"tile": table_collision_tile,
 		"recipe": null,
-		"smelting": false,
+		"crafting": false,
 		"timer": null,
-		"area": furnace_area,
+		"area": table_area,
 		"inventory": [],
 		"id": "Furnace",
 		"recipes": {
-			"Bronze_Shield_Chunk": ["Bronze_Ore", "Bronze_Ore", "Bronze_Ore"],
-			"Gold_Shield_Chunk": ["Gold_Ore", "Gold_Ore", "Gold_Ore"],
-			"Diamond_Shield_Chunk": ["Diamond_Ore", "Diamond_Ore", "Diamond_Ore"],
+			"Bronze_Sword": [&"Bronze_Blade", &"Leather_Hide"],
+			"Gold_Sword": [&"Gold_Blade", &"Leather_Hide"],
+			"Diamond_Sword": [&"Diamond_Blade", &"Leather_Hide"]
 		}
 	})
 	
@@ -104,9 +115,9 @@ func create_collision_for_single_tile(coordinates: Vector2) -> Area2D:
 
 func create_collision_for_anvil(coordinates: Vector2) -> Area2D:
 	var tile_position: Vector2 = map_to_local(coordinates)
-	var top: Vector2 = tile_position - Vector2(16, 16 / 2) + Vector2(0, -16 / 2)
-	var left: Vector2 = tile_position - Vector2(16, 16 / 2) + Vector2(-32 / 2 , 0)
-	var bottom: Vector2 = tile_position + Vector2(0, 16 / 2)
+	var top: Vector2 = tile_position + Vector2(0, -16 / 2)
+	var left: Vector2 = tile_position + Vector2(-16, 16 / 2) + Vector2(-32 / 2 , 0)
+	var bottom: Vector2 = tile_position + Vector2(-16, 16 / 2) + Vector2(0, 16 / 2)
 	var right: Vector2 = tile_position + Vector2(32 / 2, 0)
 	var area: Area2D = Area2D.new()
 	area.z_index = 1
@@ -129,20 +140,11 @@ func create_collision_for_furnace(coordinates: Vector2) -> Area2D:
 	return area
 
 func create_collision_for_table(coordinates: Vector2) -> Area2D:
-=======
-
-# Create Area2D on tile
-func create_collision_for_tile(coordinates: Vector2) -> Area2D:
->>>>>>> Stashed changes
 	var tile_position: Vector2 = map_to_local(coordinates)
 	var top: Vector2 = tile_position + Vector2(0, -16 / 2)
-	var left: Vector2 = tile_position + Vector2(-32 / 2 , 0)
-	var bottom: Vector2 = tile_position + Vector2(0, 16 / 2)
+	var left: Vector2 = tile_position + Vector2(-16, 16 / 2) + Vector2(-32 / 2 , 0)
+	var bottom: Vector2 = tile_position + Vector2(-16, 16 / 2) + Vector2(0, 16 / 2)
 	var right: Vector2 = tile_position + Vector2(32 / 2, 0)
-<<<<<<< Updated upstream
-=======
-		
->>>>>>> Stashed changes
 	var area: Area2D = Area2D.new()
 	area.z_index = 1
 	var collision_shape: CollisionPolygon2D = CollisionPolygon2D.new()
@@ -152,7 +154,6 @@ func create_collision_for_tile(coordinates: Vector2) -> Area2D:
 
 # Old Triggger for entering a crates area2d
 func area_entered(node: Node2D, emitter: Area2D):
-<<<<<<< Updated upstream
 	# Area2D colliding with something, check if our node is the blacksmith
 	# so that we don't prematurely set z_index
 	if node != $Blacksmith:
@@ -160,34 +161,15 @@ func area_entered(node: Node2D, emitter: Area2D):
 	$Blacksmith.interactable = emitter
 	if (emitter.get_groups()[0] == "Furnace"):
 		$Blacksmith.z_index = 4
-=======
-	$Blacksmith.interactable = emitter
->>>>>>> Stashed changes
 
 # Old Trigger for exiting a crates area2d
 func area_exited(node: Node2D, emitter: Area2D):
 	$Blacksmith.interactable = null
-<<<<<<< Updated upstream
 	if (emitter.get_groups()[0] == "Furnace"):
 		$Blacksmith.z_index = 1
-=======
->>>>>>> Stashed changes
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 # This should be moved to Blacksmith eventually probably
 func _process(_delta):
 	$Camera2D.position = $Blacksmith.position
-<<<<<<< Updated upstream
 
-=======
-	if Input.is_action_just_pressed("drop_item") && $Blacksmith.pickedUpItem == true:
-		var node = $Blacksmith.heldItem
-		$Blacksmith.pickedUpItem = false
-		$Blacksmith.remove_child(node)
-		add_child(node)
-		node.z_index = 1
-		node.y_sort_enabled = true
-		node.scale.x = 1
-		node.scale.y = 1
-		node.position = $Blacksmith.position
->>>>>>> Stashed changes
