0bf1d7f4fec2ad458ee86a690ea3b215c6af7bae
nv-fp <166872119+nv-fp@users.noreply.github.com>
2024-04-17 02:28:05 -0700

    sprite-based background (#12)
    Co-authored-by: nvfp <nv-fp@users.noreply.github.com>

--- nodes/ui/MaterialToast.gd
+++ nodes/ui/MaterialToast.gd
@@ -1,11 +1,13 @@
 extends Node2D
 
-# Array[[String, Sprite2D]]
+# Array[[String, Sprite2D, Sprite2D]]
 var components = []
 
 @export var bubble_spacing = 4
 @export var circle_radius = 8
 @export var circle_bg_color = Color.GRAY
+
+var bg_tex = preload('res://art/tiny-circle.png')
 
 var _center = 0
 var _left = 1
@@ -23,7 +25,7 @@ func _get_pos_x(type: int) -> int:
 func _get_pos_y(lvl: int) -> int:
 	return position.y - (lvl * (2 * (circle_radius + bubble_spacing/2)))
 
-func _draw():
+func _reflow():
 	var count = components.size()
 	if count == 0:
 		return
@@ -33,9 +35,11 @@ func _draw():
 	var cur_position = _right
 	for idx in range(0, count - 1):
 		var mat_center = Vector2(_get_pos_x(cur_position), _get_pos_y(lvl))
-		draw_circle(mat_center, circle_radius, circle_bg_color)
+		# draw_circle(mat_center, circle_radius, circle_bg_color)
 		var s = components[idx][1]
 		s.position = mat_center
+		var s_bg = components[idx][2]
+		s_bg.position = mat_center
 
 		match cur_position:
 			_left:
@@ -56,8 +60,9 @@ func _draw():
 		last = _center
 	
 	var mat_center = Vector2(_get_pos_x(last), _get_pos_y(lvl))
-	draw_circle(mat_center, circle_radius, circle_bg_color)
+	#draw_circle(mat_center, circle_radius, circle_bg_color)
 	components[count - 1][1].position = mat_center
+	components[count - 1][2].position = mat_center
 	
 # adds a new toast icon
 # comes preloaded with toast_<materail> via atlas.cfg but generally
@@ -65,16 +70,22 @@ func _draw():
 func add_material(type: String):
 	var node = Sprite2D.new()
 	node.texture = WantBubbleFactory.get_tex(type)
+	var bg_node = Sprite2D.new()
+	bg_node.texture = bg_tex
+	add_child(bg_node)
 	add_child(node)
-	components.push_back([type, node])
-	queue_redraw()
+	components.push_back([type, node, bg_node])
+	#queue_redraw()
+	_reflow()
 
 # removes all materials
 func clear():
 	for c in components:
 		remove_child(c[1])
+		remove_child(c[2])
 	components.clear()
-	queue_redraw()
+	#queue_redraw()
+	_reflow()
 
 func _add_random():
 	var mats = [
