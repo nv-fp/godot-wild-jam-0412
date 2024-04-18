8851c2eb16820e5da09c738ffcc7c9614f30f024
nv-fp <166872119+nv-fp@users.noreply.github.com>
2024-04-18 16:26:19 -0700

    fix level hierarchy assumptions (#17)
    I had assumed the LevelX.gd was hanging off LevelX.tscn. It wasn't but i made it work for 1 by adding a new one .. oops.
    Anyway, this fixes level2 and 3
    
    Co-authored-by: nvfp <nv-fp@users.noreply.github.com>

--- scripts/Main2.gd
+++ scripts/Main2.gd
@@ -9,10 +9,12 @@ extends Node2D
 	'diamond_shield'
 ]
 
+@export var score_limits = [-10000, 200, 400]
+
 # Called when the node enters the scene tree for the first time.
 func start_level():
 	$Hud.visible = true
 	$Hud.start_level(level_items)
 
 func end_level():
-	$Level.end_level()
+	$Level.get_node('TileMap').end_level()
