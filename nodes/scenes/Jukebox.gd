58ce35242a8ed873b7560168682bd6c40a1a7b3f
nv-fp <166872119+nv-fp@users.noreply.github.com>
2024-04-19 22:59:58 -0700

    misc changes (#25)
    - initial credits fuckery
    - music updates
    - want bubble updates
    
    Co-authored-by: nvfp <nv-fp@users.noreply.github.com>

--- nodes/scenes/Jukebox.gd
+++ nodes/scenes/Jukebox.gd
@@ -4,6 +4,7 @@ var playing: bool = false
 
 var bg_music = preload('res://sound/In_A_Rush_Demo-95bpm.mp3')
 var title_music = preload('res://sound/Proliferated_Edge.ogg')
+var credit_music = preload('res://sound/Meet_Me_At_The_End_Demo.mp3')
 
 var cur_music = null
 
@@ -31,6 +32,14 @@ func play_bg():
 
 func play_title():
 	_switch_to(title_music)
+
+func play_credits():
+	_switch_to(credit_music)
+
+func seek(to: float):
+	if cur_music == null:
+		return
+	player.seek(to)
 
 func _switch_to(new_stream):
 	if cur_music == new_stream:
---
+++ nodes/ui/Clouds.gd
@@ -0,0 +1,87 @@
+extends Node2D
+
+@export var height = 720 / 3
+@export var fast_clouds_vel = 1280 / 18
+@export var slow_clouds_vel =  1280 / 23
+
+@export var max_clouds = 12
+
+var fast_clouds = []
+var slow_clouds = []
+
+var tex = [
+	preload('res://art/credits/cloud1.png'),
+	preload('res://art/credits/cloud2.png'),
+	preload('res://art/credits/cloud3.png'),
+]
+
+var pool = []
+var _max_x = 0
+
+func _get_y() -> int:
+	return position.y + randi_range(0, height)
+
+func _on_screen(xy: Vector2) -> bool:
+	var t = xy.x < (_max_x + 200) 
+	return t
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	DisplayServer.window_get_size()
+	_max_x = get_viewport().get_visible_rect().size.x
+
+	for x in range(0, max_clouds):
+		var c = Sprite2D.new()
+		c.visible = false
+		pool.push_back(c)
+
+	for x in range(0, max_clouds):
+		add_child(reset_clouds())
+		for c in fast_clouds:
+			c.position.x = randi_range(-_max_x, _max_x)
+		for c in slow_clouds:
+			c.position.x = randi_range(-_max_x, _max_x)
+
+func reset_clouds():
+	if pool.size() == 0:
+		return
+
+	if (fast_clouds.size() + slow_clouds.size()) >= max_clouds:
+		return
+
+	var c = pool[0]
+	pool.remove_at(0)
+	
+	var is_fast = randi_range(0, 100) % 2 == 0
+	if is_fast:
+		fast_clouds.push_back(c)
+	else:
+		slow_clouds.push_back(c)
+	
+	var cloud_tex = tex[randi_range(0, tex.size() - 1)]
+	c.position = Vector2(-cloud_tex.get_width() - randi_range(0, 200), _get_y())
+	c.texture = cloud_tex
+	c.visible = true
+	return c
+
+func reap_clouds():
+	var fast_to_remove = fast_clouds.filter(func(c): return !_on_screen(c.position))
+	var slow_to_remove = slow_clouds.filter(func(c): return !_on_screen(c.position))
+
+	fast_clouds = fast_clouds.filter(func(c): return _on_screen(c.position))
+	slow_clouds = slow_clouds.filter(func(c): return _on_screen(c.position))
+
+	for x in fast_to_remove:
+		pool.push_back(x)
+	for x in slow_to_remove:
+		pool.push_back(x)
+
+func _process(delta):
+	reset_clouds()
+
+	for c in fast_clouds:
+		c.position.x += (delta * fast_clouds_vel)
+	for c in slow_clouds:
+		c.position.x += (delta * slow_clouds_vel)
+
+	reap_clouds()
